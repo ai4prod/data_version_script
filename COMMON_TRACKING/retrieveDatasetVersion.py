5e51af20aea0ca81a5c3a69cab1d7873bf4bff3a
Eric Tondelli <eric.tondelli@gmail.com>
2022-07-25 14:44:36 +0200

    add commmon script

--- COMMON_TRACKING/retrieveDatasetVersion.py
+++ COMMON_TRACKING/retrieveDatasetVersion.py
@@ -9,9 +9,16 @@ dataset_version=2
 p = subprocess.Popen(["git","checkout","v{}".format(str(dataset_version))], cwd=cwd)
 p.wait()
 
+if(p.returncode!=0):
+    raise Exception("Cannot find dataset_version")
 
 p = subprocess.Popen(["dvc","pull"], cwd=cwd)
 p.wait()
+
+
+if(p.returncode!=0):
+    raise Exception("Cannot retrieve dataset")
+
 
 print("RETRIEVE DATASET v{}".format(dataset_version))
 
--- COMMON_TRACKING/updateDatasetVersion.py
+++ COMMON_TRACKING/updateDatasetVersion.py
@@ -7,23 +7,37 @@ remoteDataset="/media/dati/RemoteDataset/simpleObjectDetectionVersioningRemote/"
 dataset_version=2
 
 
+# Each step need to be verified
 
 p = subprocess.Popen(["dvc","add","Data/Dataset"], cwd=cwd)
 p.wait()
 
+if(p.returncode!=0):
+    raise Exception("Cannot Use dvc")
 
 p = subprocess.Popen(["git","add","Data/Dataset.dvc","Data/.gitignore"], cwd=cwd)
 p.wait()
 
+if(p.returncode!=0):
+    raise Exception("Cannot add to git")
 
 p = subprocess.Popen(["git","commit","-m","add dataset version {}".format(str(dataset_version))], cwd=cwd)
 p.wait()
 
+
+if(p.returncode!=0):
+    raise Exception("Cannot commit to git")
+
 p = subprocess.Popen(["git","tag","-a","v{}".format(str(dataset_version)),"-m","update dataset"], cwd=cwd)
 p.wait()
+
+if(p.returncode!=0):
+    raise Exception("Cannot tag")
 
 p = subprocess.Popen(["dvc","push"], cwd=cwd)
 p.wait()
 
+if(p.returncode!=0):
+    raise Exception("Cannot Update Dataset")
 
-
+print("UPDATE DATASET TO v{} SUCCESSFUL".format(dataset_version))
